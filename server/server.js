bbaeda98dd37f77d8d116c0b54a91f2b8f79f116
Ayushcode04 <ayushsinghg04@gmail.com>
2025-02-27 17:30:37 +0530

    Deployment changes 2

--- server/server.js
+++ server/server.js
@@ -39,9 +39,10 @@ app.use(router);
 //error middleware
 app.use(errorMiddleware);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+
 })
 
 app.listen(PORT, () => {
