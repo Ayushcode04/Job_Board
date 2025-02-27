4cc240b8e8ca3d24f57aadf43f8b33190b77389e
Ayushcode04 <ayushsinghg04@gmail.com>
2025-02-27 18:07:53 +0530

    deployment changes 5

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 const config = {
     development: "http://localhost:8000/api-v1/",
-    production: "https://job-board-x771.onrender.com",
+    production: "https://job-board-x771.onrender.com/api-v1/",
   };
   
   export const BASE_URL = config[process.env.NODE_ENV] || config.development;
